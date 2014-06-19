bfdfc3f1a2cbf86de212699f0716659bc3a09cf9
Tim Collinson <trcollinson@gmail.com>
2014-06-18 23:44:48 -0600

    Adding early basic tests for teaching TDD by skeleton.

--- BowlingScore.java
+++ BowlingScore.java
@@ -1,4 +1,10 @@
 
 public class BowlingScore {
+public boolean nextScore(int score){
+	return true;
+}
 
+public int frameScore(){
+	return 1;
+}
 }
--- BowlingScoreTest.java
+++ BowlingScoreTest.java
@@ -6,8 +6,13 @@ import org.junit.Test;
 public class BowlingScoreTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testBowlingScore() {
+		BowlingScore testScore = new BowlingScore();
+		assertNotNull("testScore object should exist", testScore);
+		
+		assertTrue("nextScore should accept a value", testScore.nextScore(1));
+		
+		assertEquals("frameScore should be 1 after nextScore of 1", 1, testScore.frameScore());
 	}
 
 }
